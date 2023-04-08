fddd0afc429f5766c078ba62e7e0afff9aa04765
Hoan <NguyenTranHoan.Mr@gmail.com>
2023-04-08 17:26:11 +0700

    airflow

---
+++ project_template 3/plugins/final_project_operators/load_fact.py
@@ -0,0 +1,38 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class LoadFactOperator(BaseOperator):
+
+    ui_color = '#F98866'
+
+    @apply_defaults
+    def __init__(self,
+                 conn_id="",
+                 table="",
+                 sql="",
+                 append_only=False,
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+
+        self.conn_id = conn_id
+        self.table = table
+        self.sql = sql
+        self.append_only = append_only
+
+    def execute(self, context):
+        db = PostgresHook(postgres_conn_id=self.conn_id)
+
+        if not self.append_only:
+            self.log.info(f"Delete {self.table} fact table")
+            db.run(f"DELETE FROM {self.table}")
+
+        self.log.info(
+            f"Insert data from staging tables into {self.table} fact table")
+
+        insert_statement = f"INSERT INTO {self.table} {self.sql}"
+        self.log.info(f"Running sql: \n{insert_statement}")
+        db.run(insert_statement)
+        self.log.info(f"Successfully completed insert into {self.table}")
